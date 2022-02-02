6c95d08a4266a43dd5c4fbb929e3d00c60e39669
Mohib <mbmohib@gmail.com>
2022-02-02 21:57:27 +0600

    NEW: add radio mock data

---
+++ src/mocks/db/radio.db.ts
@@ -0,0 +1,27 @@
+export const radios = [
+  {
+    id: '01',
+    name: 'Putin FM',
+    frequency: '66,6',
+  },
+  {
+    id: '02',
+    name: 'Dribbble FM',
+    frequency: '101,2',
+  },
+  {
+    id: '03',
+    name: 'Doge FM',
+    frequency: '99,4',
+  },
+  {
+    id: '04',
+    name: 'Ballads FM',
+    frequency: '87,1',
+  },
+  {
+    id: '05',
+    name: 'Maximum FM',
+    frequency: '142,2',
+  },
+];
